556ab8359b7ec7e492a5ea0ab57852eb240403bf
esmurphy7 <esmurphy7@gmail.com>
2014-03-12 17:48:34 -0700

    updated MainActivity.java

--- App/src/com/example/app/MainActivity.java
+++ App/src/com/example/app/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.app;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends SimpleBaseGameActivity {
+
+	private static final int CAMERA_WIDTH = 480;
+	private static final int CAMERA_HEIGHT = 320;
+	
+	private BoundCamera mBoundCamera;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public EngineOptions onCreateEngineOptions() {
+		this.mBoundCamera = new BoundCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mBoundCamera);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
